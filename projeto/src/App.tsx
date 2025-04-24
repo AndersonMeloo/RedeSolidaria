a61b6e623e1450179fd3cd39b4e8536af266a181
Anderson Melo <142186594+AndersonMeloo@users.noreply.github.com>
2025-04-23 23:01:51 -0300

    Update App.tsx

--- projeto/src/App.tsx
+++ projeto/src/App.tsx
@@ -3,13 +3,12 @@ import { Header } from "./components/Contato"
 function App() {
 
   return (
-    <>
 
+    <>
       <Header>Home</Header>
       <Header>Informações</Header>
       <Header>Contato</Header>
       <Header>Doe</Header>
-
     </>
   )
 }
