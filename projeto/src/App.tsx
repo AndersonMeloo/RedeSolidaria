e5a17db226b51b2bbbd13ac61b0c1286ee1cbc7d
Anderson Melo <142186594+AndersonMeloo@users.noreply.github.com>
2025-05-05 21:23:18 -0300

    a

--- projeto/src/App.tsx
+++ projeto/src/App.tsx
@@ -14,15 +14,6 @@ function App() {
         <h1>Rede Solidaria</h1>
       </Doacao>
 
-      {/* <Header idade={27} name="Anderson" /> */}
-
-      {/* <Imagem imagem="https://images.summitmedia-digital.com/topgear/images/2024/01/24/1-1706068083.jpg" /> */}
-
-      {/* <Atualizar/> */}
-
-      {/* <Doacoes /> */}
-
-
     </>
   )
 }
