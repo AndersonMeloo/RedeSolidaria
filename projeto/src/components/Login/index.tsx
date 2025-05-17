f257ed8d82e7947f64b9750814baea9a74e16a36
Anderson Melo <142186594+AndersonMeloo@users.noreply.github.com>
2025-05-17 12:58:14 -0300

    alt

--- projeto/src/components/Login/index.tsx
+++ projeto/src/components/Login/index.tsx
@@ -1,11 +1,33 @@
+// import videoLogin from '../../../imgs/vid/Animation - 1747364139322.webm'
+import imgLogin from '../../../imgs/icon-conta.png'
+
+import sass from './sass.module.scss'
+
 function Login() {
 
-    return ( 
+    return (
 
         <>
-        
+            <div className={sass.container}>
+
+                <div className={sass.login}>
+                    <img src={imgLogin} alt="" />
+                </div>
+
+                <div className={sass.teste}>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia qui, dolores nostrum corporis officia perferendis consequatur, molestias quaerat commodi odio alias nihil. Voluptatum voluptatem omnis minus eaque illum, nemo aliquid!
+                    </p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia qui, dolores nostrum corporis officia perferendis consequatur, molestias quaerat commodi odio alias nihil. Voluptatum voluptatem omnis minus eaque illum, nemo aliquid!
+                    </p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia qui, dolores nostrum corporis officia perferendis consequatur, molestias quaerat commodi odio alias nihil. Voluptatum voluptatem omnis minus eaque illum, nemo aliquid!
+                    </p>
+                </div>
+
+            </div>
+
+            {/* <video src={videoLogin}></video> */}
         </>
-     );
+    );
 }
 
 export default Login;
