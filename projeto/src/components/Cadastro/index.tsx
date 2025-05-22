5ed1a059428075f1c9e60c0832462d945b719124
Anderson Melo <142186594+AndersonMeloo@users.noreply.github.com>
2025-05-21 21:57:37 -0300

    alt

--- projeto/src/components/Cadastro/index.tsx
+++ projeto/src/components/Cadastro/index.tsx
@@ -1,4 +1,5 @@
-import DefaultButton from "../DefaultButton";
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
+// import DefaultButton from "../DefaultButton";
 import DefaultInput from "../DefaultInput";
 import sass from './sass.module.scss'
 
@@ -8,22 +9,27 @@ function Cadastro() {
 
         <>
 
-            <form action="">
+            <div className={sass.containerForm}>
+                <form action="">
+                    <div className={sass.registrar}>
+                        <DotLottieReact
+                            src="https://lottie.host/d08a262e-97c6-44d4-bc97-f2926d8f24bc/zgZR2bhOUm.lottie"
+                            loop
+                            autoplay
+                        />
+                    </div>
 
-                <div className={sass.cadastro}>
+                    <div className={sass.cadastro}>
+                        <DefaultInput id="" labelText="Nome" type="text" required />
+                        <DefaultInput id="" labelText="Ano Nascimento" type="date" />
+                        <DefaultInput id="" labelText="E-mail" placeholder="user1234@" type="email" required />
+                        <DefaultInput id="" labelText="Telefone" type="tel" placeholder="(00) 00000-0000" required />
+                        <DefaultInput id="" labelText="Senha" type="password" required />
+                        <DefaultInput id="" labelText="Confirmar Senha" type="password" required />
+                    </div>
+                </form>
+            </div>
 
-                    <DefaultInput id="" labelText="Nome" type="text" required />
-                    <DefaultInput id="" labelText="Ano Nascimento" type="date" />
-                    <DefaultInput id="" labelText="E-mail" placeholder="user1234@" type="email" required />
-                    <DefaultInput id="" labelText="Telefone" type="tel" placeholder="(00) 00000-0000" required />
-                    <DefaultInput id="" labelText="Senha" type="password" required />
-                    <DefaultInput id="" labelText="Confirmar Senha" type="password" required />
-
-                    <DefaultButton id="">
-                        <button type="submit">Cadastrar</button>
-                    </DefaultButton>
-                </div>
-            </form>
         </>
     );
 }
