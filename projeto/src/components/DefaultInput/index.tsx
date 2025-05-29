8309de33dcee581f11108877b05bed9ec828b473
Anderson Melo <142186594+AndersonMeloo@users.noreply.github.com>
2025-05-29 18:49:37 -0300

    Api Criada

--- projeto/src/components/DefaultInput/index.tsx
+++ projeto/src/components/DefaultInput/index.tsx
@@ -1,23 +1,30 @@
-import sass from './sass.module.scss'
-// import imgLogin from '../../../imgs/icon-conta.png''
+import React from 'react';
+import sass from './sass.module.scss';
 
 type DefaultInputProps = {
+  id: string;
+  labelText: string;
+} & React.InputHTMLAttributes<HTMLInputElement>;
 
-    id: string,
-    labelText: string,
-} & React.ComponentProps<'input'>
-
-function DefaultInput({ id, labelText, type, ...rest }: DefaultInputProps) {
-
-    return (
-        <>
-        
-            <div className={sass.defaultInput}>
-                <label htmlFor={id} className={sass.label}>{labelText}</label>
-                <input type={type} id={id} {...rest} className={sass.input} />
-            </div>
-        </>
-    );
-}
+const DefaultInput: React.FC<DefaultInputProps> = ({
+  id,
+  labelText,
+  type,
+  ...rest
+}) => {
+  return (
+    <div className={sass.defaultInput}>
+      <label htmlFor={id} className={sass.label}>
+        {labelText}
+      </label>
+      <input
+        id={id}
+        type={type}
+        className={sass.input}
+        {...rest} // Inclui value, onChange, required, placeholder etc.
+      />
+    </div>
+  );
+};
 
 export default DefaultInput;
--- projeto/src/components/Sobre/index.tsx
+++ projeto/src/components/Sobre/index.tsx
@@ -1,10 +1,46 @@
+import sass from './sass.module.scss'
+
+import imgDonation from '../../../imgs/donation.png'
+
 function Sobre() {
-    return ( 
+    return (
 
         <>
-        
+
+            <div className={sass.containerSobre}>
+
+                <div className={sass.containerTextos}>
+                    <h1>Sobre Nós</h1>
+                    <p>A Rede Solidária é uma iniciativa criada com o
+                        propósito de unir pessoas dispostas a fazer a
+                        diferença na vida de quem mais precisa. Nosso
+                        site é uma plataforma dedicada à solidariedade
+                        , onde você pode contribuir de diversas formas
+                        para ajudar famílias e indivíduos em situação de
+                        vulnerabilidade social.
+                        <br />
+                        <br />
+                        Nosso objetivo é arrecadar alimentos, roupas,
+                        brinquedos, produtos de higiene, além de prom-
+                        over serviços voluntários como cortes de cabelo,
+                        atendimento psicológico, suporte jurídico, entre
+                        outros. Cada doação, por menor que pareça, tem
+                        um impacto enorme na vida de quem a recebe.
+                        Acreditamos que juntos somos mais fortes. Por
+                        isso, a Rede Solidária é mais do que um site – é uma
+                        comunidade do bem, formada por pessoas que se
+                        importam e querem transformar o mundo em um
+                        lugar mais justo e humano.
+                        Junte-se a nós nessa corrente de amor e empatia.
+                        Sua ajuda pode mudar vidas!</p>
+                </div>
+
+                <div>
+                    <img src={imgDonation} alt="" />
+                </div>
+            </div>
         </>
-     );
+    );
 }
 
 export default Sobre;
